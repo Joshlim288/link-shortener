b48b8221a856a9352ae486f7cb4f3a3bc9d87358
Josh <joshlim288@gmail.com>
2022-09-20 00:26:43 +0800

    implement initial API endpoints

--- Backend/app/api_controller.py
+++ Backend/app/api_controller.py
@@ -5,9 +5,30 @@ Contains the API definitions to request data from the server
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+# project imports
+from shortener_service import *
+
 app = Flask(__name__)
 CORS = CORS(app)
 
 @app.route("/")
 def landing_page():
     return "<p>URL shortening server</p>"
+
+'''
+shorten the provided url, and get the assigned base62code
+'''
+@app.route("shorten", methods=['GET'])
+def shorten():
+    args = request.args
+    url = args.get('url')
+    return shorten(url)
+
+'''
+get the url corresponding to the base62code previously issued
+'''
+@app.route("retrieve", methods=['GET'])
+def shorten():
+    args = request.args
+    base62code = args.get('base62code')
+    return retrieve(base62code)
