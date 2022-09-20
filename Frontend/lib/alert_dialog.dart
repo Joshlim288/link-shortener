0dd864f265ad5c66da67db94f9d7a3442c54e04e
Josh <joshlim288@gmail.com>
2022-09-20 22:09:52 +0800

    Implement copy to clipboard functionality

--- Frontend/lib/alert_dialog.dart
+++ Frontend/lib/alert_dialog.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/api_connection.dart';
+import 'package:flutter/services.dart';
 
 /// Contains logic for the dialogs shown in the application
 /// Supports dialogs with and without input
@@ -19,6 +19,7 @@ class UrlAlertDialogState extends State<UrlAlertDialog> {
   int? responseCode;
   Widget? title, body;
   TextStyle titleStyle = const TextStyle(fontWeight: FontWeight.bold);
+  String frontendUrl = (const String.fromEnvironment('FRONTEND_URL')).length != 0 ? const String.fromEnvironment('FRONTEND_URL') : "http://localhost:8080/";
 
   void callback(String response, int statusCode) {
     setState(() {
@@ -48,11 +49,7 @@ class UrlAlertDialogState extends State<UrlAlertDialog> {
         // Request error
         title = Column(
           children: const [
-            Icon(
-              Icons.error,
-              color: Colors.red,
-              size: 30,
-            ),
+            Icon(Icons.error, color: Colors.red, size: 30),
             SizedBox(height: 5),
             Text('Request Failed'),
           ],
@@ -63,28 +60,29 @@ class UrlAlertDialogState extends State<UrlAlertDialog> {
         );
       } else {
         // Request succeeded, display shortened url
+        responseText = '$frontendUrl#/${responseText!}';
         title = Column(
           children: const <Widget>[
-            Icon(
-              Icons.check_circle,
-              color: Colors.green,
-              size: 30,
-            ),
+            Icon(Icons.check_circle, color: Colors.green, size: 30),
             Text('Success'),
           ],
         );
         body = Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
               initialValue: responseText,
               readOnly: true,
               maxLines: 1,
             ),
-            ElevatedButton(onPressed: () {}, child: const Text('Copy to clipboard'))
           ],
         );
       }
     }
+
     return AlertDialog(
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(10)),
@@ -92,6 +90,25 @@ class UrlAlertDialogState extends State<UrlAlertDialog> {
       title: Center(child: title),
       content: body,
       actions: <Widget>[
+        if (responseCode == 200)
+          TextButton(
+            onPressed: () {
+              setState(() {
+                Clipboard.setData(ClipboardData(text: responseText));
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 6, 0, 6),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const <Widget>[
+                  Icon(Icons.copy),
+                  SizedBox(width: 10),
+                  Text('Copy to Clipboard', style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+          ),
         TextButton(
           onPressed: () => Navigator.pop(context),
           child: const Text(
