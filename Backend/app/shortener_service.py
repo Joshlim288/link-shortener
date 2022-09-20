119db9b5d60c37189a241d92c1cb64546da50bce
Josh <joshlim288@gmail.com>
2022-09-20 14:48:13 +0800

    Implement initial url shortening and retrieval logic

--- Backend/app/shortener_service.py
+++ Backend/app/shortener_service.py
@@ -1,24 +1,58 @@
 '''
 SHORTENER SERVICE
-Single responsibility of performing the actual main business logic for the application, handling shortened URLs
+Single responsibility of handling shortened URLs, and calling the appropriate services to perform a high-level use case
 '''
+# library imports
+import validators
+import data_access
+
+# Constants
+ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+CODE_LENGTH = 7
+'''
+Handles the conversion from document ID (base10) to shortened code (currently base62)
+We currently limit the result to be 7 characters long, to ensure url remains short. 26^7 can handle about 8 billion unique urls
+We only need a one-way conversion, actual document ID is not used for anything at the moment
+'''
+def encode(num):
+    arr = []
+    base = len(ALPHABET)
+    while num:
+        num, rem = divmod(num, base)
+        arr.append(ALPHABET[rem]) 
+    # reversing the arr would be more accurate, but it doesn't matter for our use case, just needs to be unique
+    return ''.join(arr).ljust(CODE_LENGTH, ALPHABET[0])
 
 '''
-Shortens a url to base62 format (a-z, A-Z, 0-9)
-We currently limit the result to be 7 characters long, to ensure url remains short. 26^7 can handle about 8 billion unique urls
+Shortens a url to generate a unique short code corresponding to that website
 We want to reduce collisions, and thus simply generate the IDs in order instead of hashing the url
 If the url already exists in the database, return the same string to avoid redundancy
+Returns (Message, StatusCode)
 '''
 def shorten(url):
     # validate url
+    if not validators.url(url):
+        return 'Not a valid URL', 400
+    
     # check url exists in database -> we do this to maximise storage space
-    # get latest id, add 1 and create the base62code
-    pass
+    encodedId = data_access.getByLongUrl(url)
+    if encodedId:
+        return encodedId, 200
+
+    # get next id, and create the base62code
+    nextId = data_access.getNextId()
+    encodedId = encode(nextId)
+    data_access.addNewId(encodedId, url)
+    return encodedId, 200
+    
 
 '''
 Returns a url when given a base62code from a previously shortened url
+No need to validate the code, existence in the database is all that is needed at this point
 '''
 def retrieve(base62code):
-    # validate base62 code
     # return corresponding url if exists in database
-    pass
+    doc = data_access.getByBase62Code(base62code)
+    if not doc:
+        return 'Invalid url entered', 400
+    return doc['originalUrl'], 200
