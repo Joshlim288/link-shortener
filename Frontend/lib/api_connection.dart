d7b8584c7d250414ff66194a200c97e4a51530a5
Josh <joshlim288@gmail.com>
2022-09-20 19:57:04 +0800

    Change callback functions to take in statusCode as well

--- Frontend/lib/api_connection.dart
+++ Frontend/lib/api_connection.dart
@@ -8,24 +8,24 @@ class ApiConnection {
   static String baseUrl = (const String.fromEnvironment('BACKEND_URL')).length != 0 ? const String.fromEnvironment('BACKEND_URL') : "http://localhost:5000/";
 
   /// Enter a new batch of teams into the system
-  static Future<void> shortenUrl(String url, Function(String) callback) async {
+  static Future<void> shortenUrl(String url, Function(String, int) callback) async {
     var request = http.Request('GET', Uri.parse('$baseUrl/shorten?url=$url'));
     sendRequest(request, callback);
   }
 
   /// enter match results into the system
-  static Future<void> retrieveUrl(String base62code, Function(String) callback) async {
+  static Future<void> retrieveUrl(String base62code, Function(String, int) callback) async {
     var request = http.Request('POST', Uri.parse('$baseUrl/retrieve?base62code=$base62code'));
     sendRequest(request, callback);
   }
 
   /// Abstract out code that will be sent for every request
-  static Future<void> sendRequest(var request, Function(String) callback) async {
+  static Future<void> sendRequest(var request, Function(String, int) callback) async {
     try {
       http.StreamedResponse response = await request.send().timeout(const Duration(seconds: 10));
-      callback(await response.stream.bytesToString());
+      callback(await response.stream.bytesToString(), response.statusCode);
     } on TimeoutException catch (e) {
-      callback("Request timed out");
+      callback("Request timed out", 400);
     }
   }
 }
